e687219c6b0dc78fc2d4139f06795d5a272aebff
Anuj <anujshrivastav530@gmail.com>
2026-01-02 01:42:28 +0530

    fix: add ESM __dirname workaround in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
+import path from "path"; // Required for path.resolve
+import { fileURLToPath } from "url"; // Required to mimic __dirname in ESM
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import flowbiteReact from "flowbite-react/plugin/vite";
+
+// --- ESM __dirname Fix ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// --------------------------
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
+    },
+  },
 })
